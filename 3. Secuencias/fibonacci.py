af07487f3af7625ab492daa610b1de71bbfa24b5
josedaniel973 <68296032+josedaniel973@users.noreply.github.com>
2020-08-01 20:58:27 -0500

    Update and rename test.md to fibonacci.py

---
+++ 3. Secuencias/fibonacci.py
@@ -0,0 +1,13 @@
+# Algoritmo para obtener el n-ésimo número de Fibonacci.
+anterior2, anterior1, actual = 0, 0, 1
+bus = int(input("Posición del número de fibonacci a buscar: "))
+k = bus
+if bus == 1:
+    print("El primer número de fibonacci es " + str(actual) + ".")
+else:
+    while bus != 0:
+        anterior1 = actual
+        actual = anterior1 + anterior2
+        anterior2 = anterior1
+        bus -= 1
+    print("El valor del número de Fibonacci que se encuentra en la posición " + str(k) + " vale " + str(actual) + ".")
